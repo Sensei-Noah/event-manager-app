b4a117fc054dc2f46255adb8a037f55c35080344
Sensei-Noah <98984612+Sensei-Noah@users.noreply.github.com>
2022-06-12 00:42:07 +0300

    A fix/hack for production styles bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
